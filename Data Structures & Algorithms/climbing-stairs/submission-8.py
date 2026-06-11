471fd5ac9ab7b36e43e91cf362131ca56d76ead6
mcmcgovern <64622823+mcmcgovern@users.noreply.github.com>
2026-06-11 06:58:40 -0400

    Add: climbing-stairs - submission-8

---
+++ Data Structures & Algorithms/climbing-stairs/submission-8.py
@@ -0,0 +1,14 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 2:
+            return n
+
+        one_before = 0
+        two_before = 1
+        i = 2
+        while i <= n+1:
+            # temp = two_before
+            two_before, one_before = one_before + two_before, two_before
+            # one_before = temp
+            i += 1
+        return two_before
