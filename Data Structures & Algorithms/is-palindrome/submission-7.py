f97a971b5f2766e7a6b1e7c23009e93a207d631e
mcmcgovern <64622823+mcmcgovern@users.noreply.github.com>
2026-07-21 07:03:19 -0400

    Add: is-palindrome - submission-7

---
+++ Data Structures & Algorithms/is-palindrome/submission-7.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        left, right = 0, len(s)-1
+
+        while left < right:
+            # skip all non-alphanumeric characters
+            # protect against out of bounds
+            while left < right and not s[left].isalnum():
+                left += 1
+
+            while left < right and not s[right].isalnum():
+                right -= 1
+
+            # compare lowercase variants
+            if s[left].lower() != s[right].lower():
+                return False
+            left += 1
+            right -= 1
+
+        return True
