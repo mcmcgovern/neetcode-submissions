2a3d169072aebb63e125887be80495f0fa2db05f
mcmcgovern <64622823+mcmcgovern@users.noreply.github.com>
2026-07-25 18:18:16 -0400

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,14 @@
+variable = 1
+print(type(variable))
+variable = 1.0
+
+print(type(variable))
+variable = True
+
+print(type(variable))
+variable = 'test'
+
+print(type(variable))
+variable = []
+
+print(type(variable))
