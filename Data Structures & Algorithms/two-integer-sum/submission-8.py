7b13e1c7739de6c2c12f0e574c8b1e6a26045a8f
mcmcgovern <64622823+mcmcgovern@users.noreply.github.com>
2026-09-12 21:45:35 -0400

    Add: two-integer-sum - submission-8

---
+++ Data Structures & Algorithms/two-integer-sum/submission-8.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # optimal uses diff
+        diffs = defaultdict(int) # map val:index
+        for i, num in enumerate(nums):
+            diff = target - num
+            if diff in diffs:
+                return [diffs[diff], i]
+            diffs[num] = i
